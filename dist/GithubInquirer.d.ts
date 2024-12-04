18210a11ada8aa7c766bce961494eb73e0c4b33f
simonegaffurini <simone.gaffurini@regestaitalia.it>
2024-12-04 09:41:25 +0100

    update core

--- dist/GithubInquirer.d.ts
+++
@@ -1,8 +0,0 @@
-import { IInquirer, Question } from "trm-core";
-export declare class GithubInquirer implements IInquirer {
-    constructor();
-    prompt(arg1: Question | Question[]): Promise<any>;
-    setPrefix(text: string): void;
-    removePrefix(): void;
-    getPrefix(): string;
-}
