fb960caee2e5949bc3cedfaa44459bc7e829c32b
Simone Gaffurini <simone.gaffurini@regestaitalia.it>
2025-10-26 20:21:39 +0100

    update core

--- src/index.ts
+++ src/index.ts
@@ -60,7 +60,7 @@ main({
     releaseTransportTarget: getInput("releaseTransportTarget", { required: true, trimWhitespace: true }),
     customizingTransports: getInput("customizingTransports", { required: false, trimWhitespace: true })
 }).then(result => {
-    Logger.success(`+ ${result.trmPackage.manifest.get().name} ${result.trmPackage.manifest.get().version}`);
+    Logger.success(`+ ${result.trmPackage.manifest.get().name} v${result.trmPackage.manifest.get().version}`);
 }).catch(e => {
     setFailed(e);
 });
